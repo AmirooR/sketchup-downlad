dcd975fd1249fbd1cee68f72aafb7f20d54b73dd
AmirooR <noname01.cpp@gmail.com>
2013-10-09 10:42:07 +0330

    download function

--- read3dWareHouse.py
+++ read3dWareHouse.py
@@ -1,48 +1,36 @@
 # -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
 
 import urllib2
 from bs4 import BeautifulSoup
 import numpy as np
 from sklearn.externals import joblib
 
-# <codecell>
+def downloadAllModels( query_word, num_pages):
+	for i in range(num_pages):
+		print 'Download page %d' % (i+1)
+		downloadModelWithPage( query_word, i)
 
-url = 'http://sketchup.google.com/3dwarehouse/search?q=bus&styp=m&scoring=t&btnG=Search&reps=1&start=12'
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
+def downloadModelWithPage( query_word, num_page):
+	url = 'http://sketchup.google.com/3dwarehouse/search?q='+query_word+'&styp=m&scoring=t&btnG=Search&reps=1&start='+str( num_page*12)
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	namesPage = response.read()
+	soup = BeautifulSoup(namesPage)
+	ahrefs = soup.findAll('a')
 
-# <codecell>
-
-namesPage = response.read()
-soup = BeautifulSoup(namesPage)
-
-# <codecell>
-
-ahrefs = soup.findAll('a')
-
-# <codecell>
-
-l = []
-
-# <codecell>
-
-i = 1;
-for a in ahrefs:
-    entry = str(a)
-    if entry.find('class="dwnld"') != -1:
-        l.append(a)
-        down_url = 'http://sketchup.google.com'+a.get('href')
-        req2 = urllib2.Request(down_url)
-        response2 = urllib2.urlopen(req2)
-        filename = response2.info()['Content-Disposition'].split('filename=')[1][1:-1]
-        print 'Downloading %.1f KB to %s' % (int(response2.info()['Content-Length'])/1000.0, filename)
-        page = response2.read()
-        with open(filename,'wb') as f:
-            f.write(page)
-
-# <codecell>
+	i = 1;
+	for a in ahrefs:
+    		entry = str(a)
+    		if entry.find('class="dwnld"') != -1:
+        		down_url = 'http://sketchup.google.com'+a.get('href')
+        		req2 = urllib2.Request(down_url)
+        		response2 = urllib2.urlopen(req2)
+        		filename = response2.info()['Content-Disposition'].split('filename=')[1][1:-1]
+        		print 'Downloading %.1f KB to %s' % (int(response2.info()['Content-Length'])/1000.0, filename)
+        		page = response2.read()
+        		with open(filename,'wb') as f:
+            			f.write(page)
 
 
+if __name__ == '__main__':
+	downloadAllModels('train',1)
